f9890fbe3c24e8e4854e3705416a87dae64a2912
Katie <kjo@live.ca>
2016-06-28 20:36:57 +0200

    Generate folder name for each backup

--- src/backups.c
+++ src/backups.c
@@ -6,21 +6,24 @@
 #include <err.h>
 #include <errno.h>
 
+#include "string_manipulation.h"
+
 #define MAX_DATE_SIZE 40
 
 // Make backups for a single configuration
 static int make_backups_sub(const backup_config *conf);
 
 // Get folder name for a backup
-static int get_folder_name(char *folder_name, const backup_config *conf, const time_t curr_unix_time);
+static int get_folder_name(char *folder_name, const backup_config *conf,
+                           const time_t curr_unix_time);
 
 // Check and make new backups if required
 int make_backups(const backup_config *conf, FILE *output) {
   // Loop through list of backup configuratons
   while (conf != NULL) {
     fprintf(output, "Making backup %s...\n", conf->name);
-    if (make_backups_sub(conf)!=EXIT_SUCCESS) {
-      fprintf(stderr, "Error creating backup %s. Exiting...\n",conf->name);
+    if (make_backups_sub(conf) != EXIT_SUCCESS) {
+      fprintf(stderr, "Error creating backup %s. Exiting...\n", conf->name);
       return EXIT_FAILURE;
     }
     conf = conf->next_backup;
@@ -28,36 +31,40 @@ int make_backups(const backup_config *conf, FILE *output) {
   return EXIT_SUCCESS;
 }
 
-static int get_folder_name(char* folder_name, const backup_config *conf, const time_t curr_unix_time) {
+static int get_folder_name(char *folder_name, const backup_config *conf,
+                           const time_t curr_unix_time) {
   struct tm curr_time = *localtime(&curr_unix_time);
-  char date_string[MAX_DATE_SIZE+1];
-  date_string[MAX_DATE_SIZE] = '\0';
+  char date_string[MAX_DATE_SIZE + 1];
+  date_string[0] = '\0';
 
-  // Make folder name
+  // Write date
   size_t date_size =
       strftime(date_string, MAX_DATE_SIZE, conf->date_format, &curr_time);
   if (date_size == MAX_DATE_SIZE) {
     return EXIT_FAILURE;
   }
 
+  // Allocate and copy folder prefix
+  const char *strings[] = {conf->folder_prefix, date_string};
+  if ((folder_name = concat_and_copy_strings(strings, 2)) == NULL) {
+    return EXIT_FAILURE;
+  }
 
-  const size_t n = strlen(conf->folder_prefix) + date_size + 1;
-  folder_name = calloc(n, sizeof(char));
-
-  snprintf(folder_name, n, "%s%s", conf->folder_prefix, date_string);
+  printf("%s\n",folder_name);
 
   return EXIT_SUCCESS;
 }
 
+// Make backups for a single configuration
 static int make_backups_sub(const backup_config *conf) {
   char *folder_name = NULL;
 
   // Get current time
   time_t curr_unix_time;
-    tzset();
-    curr_unix_time = time(NULL);
+  tzset();
+  curr_unix_time = time(NULL);
 
-  if (get_folder_name(folder_name, conf, curr_unix_time)!=EXIT_SUCCESS) {
+  if (get_folder_name(folder_name, conf, curr_unix_time) != EXIT_SUCCESS) {
     return EXIT_FAILURE;
   }
 
